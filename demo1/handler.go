1228ca5b64edd0131d3190e178e89d15190fb1e7
Waldemar Bira <waldek.bira@gmail.com>
2019-05-22 13:45:32 +0200

    Updated makefile

--- demo1/handler.go
+++ demo1/handler.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 )
 func Handler(ctx context.Context) (int, error) {
+	fmt.Print(ctx)
 	httpClient := xray.Client(http.DefaultClient)
 	resp, err := ctxhttp.Get(ctx, httpClient, "http://example.com/")
 	return resp.StatusCode, err
